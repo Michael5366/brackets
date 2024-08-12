6148a63351b9caf05afcdff7975d710349b7dddc
Michael5366 <qwentin536@gmail.com>
2024-08-13 02:46:54 +0300

    feat: finished the brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const isEmpty = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      const open = bracketsConfig[j][0];
+      const close = bracketsConfig[j][1];
+
+      if (char === open) {
+        if (open === close && isEmpty[isEmpty.length - 1] === char) {
+          isEmpty.pop();
+        } else {
+          isEmpty.push(char);
+        }
+      } else if (char === close) {
+        if (isEmpty.length === 0 || isEmpty.pop() !== open) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return isEmpty.length === 0;
 }
